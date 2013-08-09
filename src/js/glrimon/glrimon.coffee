c13de7afa486cd7d378a4140a69afa7762ae77f4
Terry Brown <terry_n_brown@yahoo.com>
2013-08-09 14:35:41 -0500

    convert Layers sample to coffeescript

--- src/js/glrimon/glrimon.coffee
+++ src/js/glrimon/glrimon.coffee
@@ -4,68 +4,37 @@ map = {}
 
 layer_url = "http://umd-cla-gis01.d.umn.edu:6080/arcgis/rest/services/NRRI/glritest000/MapServer"
 
-require ["esri/map", "esri/layers/FeatureLayer",
-        "esri/dijit/AttributeInspector",
-        "esri/symbols/SimpleLineSymbol",
-        "esri/symbols/SimpleFillSymbol", "dojo/_base/Color",
-        "esri/layers/ArcGISDynamicMapServiceLayer", "esri/config",
-        "esri/tasks/query", "dojo/parser", "dojo/dom-construct",
-        "dijit/form/Button", "dijit/layout/BorderContainer",
-        "dijit/layout/ContentPane", "dojo/domReady!" ], (Map,
-        FeatureLayer, AttributeInspector, SimpleLineSymbol,
-        SimpleFillSymbol, Color, ArcGISDynamicMapServiceLayer,
-        esriConfig, Query, parser, domConstruct, Button) ->
- 
-    # parser.parse()
-        
-    esriConfig.defaults.io.proxyUrl = "/proxy"
- 
-    map = new Map 'mapDiv',
-        basemap: "topo"
-        center: [-88, 44]
-        zoom: 7
+require ["esri/map", "esri/layers/FeatureLayer", "esri/dijit/Legend",
+"dojo/_base/array", "dojo/parser",
+"dijit/layout/BorderContainer", "dijit/layout/ContentPane", 
+"dijit/layout/AccordionContainer", "dojo/domReady!"], 
+(Map, FeatureLayer, Legend, arrayUtils, parser) ->
+    parser.parse();
     
-    map.on "layers-add-result", (evt) ->
-    
-        console.log "layers-add-result"
-    
-        # layerInfo = arrayUtils.map evt.layers, (layer, index) ->
-        #     layer:layer.layer
-        #     title:layer.layer.name
-        
-        layer = evt.layers[0].layer
-        selectQuery = new Query()
-        
-        layerInfos = [{
-            'featureLayer': layer,
-            'showAttachments': false,
-            'isEditable': true,
-            'fieldInfos': [
-              {'fieldName': 'activeprod', 'isEditable':true, 'tooltip': 'Current Status', 'label':'Status:'},
-              {'fieldName': 'field_name', 'isEditable':true, 'tooltip': 'The name of this oil field', 'label':'Field Name:'},
-              {'fieldName': 'approxacre', 'isEditable':false,'label':'Acreage:'},
-              {'fieldName': 'avgdepth', 'isEditable':false, 'label':'Average Depth:'},
-              {'fieldName': 'cumm_oil', 'isEditable':false, 'label':'Cummulative Oil:'},
-              {'fieldName': 'cumm_gas', 'isEditable':false, 'label':'Cummulative Gas:'}
-            ]
-          }];
+    map = new Map "map",
+        basemap:"topo",
+        center: [-96.53, 38.374],
+        zoom: 13
 
-                
+    rivers = new FeatureLayer "http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Hydrography/Watershed173811/MapServer/1",
+        mode: FeatureLayer.MODE_ONDEMAND,
+        outFields: ["*"]
+    
+    waterbodies = new FeatureLayer "http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Hydrography/Watershed173811/MapServer/0",
+        mode: FeatureLayer.MODE_ONDEMAND,
+        outFields: ["*"]
+   
+    map.on "layers-add-result", (evt) ->
+        layerInfo = arrayUtils.map evt.layers, (layer, index) ->
+            layer:layer.layer
+            title:layer.layer.name
+    
         if layerInfo.length > 0
-        
-            console.log layerInfo
-        
             legendDijit = new Legend
                 map: map,
-                layerInfos: []  layerInfo
+                layerInfos: layerInfo
               , "legendDiv"
-            legendDijit.startup()
-            
-            attInspector = new AttributeInspector layerInfos: layerInfos, domConstruct.create "div"
-              
-            map.infoWindow.setContent attInspector.domNode
+            legendDijit.startup();
 
-    layer = new ArcGISDynamicMapServiceLayer layer_url
-    
-    map.addLayers [layer]
-    
+    map.addLayers [waterbodies, rivers]
+
--- src/js/glrimon/glrimon.js
+++ src/js/glrimon/glrimon.js
@@ -5,68 +5,37 @@ map = {};
 
 layer_url = "http://umd-cla-gis01.d.umn.edu:6080/arcgis/rest/services/NRRI/glritest000/MapServer";
 
-require(["esri/map", "esri/layers/FeatureLayer", "esri/dijit/AttributeInspector", "esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol", "dojo/_base/Color", "esri/layers/ArcGISDynamicMapServiceLayer", "esri/config", "esri/tasks/query", "dojo/parser", "dojo/dom-construct", "dijit/form/Button", "dijit/layout/BorderContainer", "dijit/layout/ContentPane", "dojo/domReady!"], function(Map, FeatureLayer, AttributeInspector, SimpleLineSymbol, SimpleFillSymbol, Color, ArcGISDynamicMapServiceLayer, esriConfig, Query, parser, domConstruct, Button) {
-  var attInspector, layer, legendDijit;
-  esriConfig.defaults.io.proxyUrl = "/proxy";
-  map = new Map('mapDiv', {
+require(["esri/map", "esri/layers/FeatureLayer", "esri/dijit/Legend", "dojo/_base/array", "dojo/parser", "dijit/layout/BorderContainer", "dijit/layout/ContentPane", "dijit/layout/AccordionContainer", "dojo/domReady!"], function(Map, FeatureLayer, Legend, arrayUtils, parser) {
+  var rivers, waterbodies;
+  parser.parse();
+  map = new Map("map", {
     basemap: "topo",
-    center: [-88, 44],
-    zoom: 7
+    center: [-96.53, 38.374],
+    zoom: 13
+  });
+  rivers = new FeatureLayer("http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Hydrography/Watershed173811/MapServer/1", {
+    mode: FeatureLayer.MODE_ONDEMAND,
+    outFields: ["*"]
+  });
+  waterbodies = new FeatureLayer("http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Hydrography/Watershed173811/MapServer/0", {
+    mode: FeatureLayer.MODE_ONDEMAND,
+    outFields: ["*"]
   });
   map.on("layers-add-result", function(evt) {
-    var layer, layerInfos, selectQuery;
-    console.log("layers-add-result");
-    layer = evt.layers[0].layer;
-    selectQuery = new Query();
-    return layerInfos = [
-      {
-        'featureLayer': layer,
-        'showAttachments': false,
-        'isEditable': true,
-        'fieldInfos': [
-          {
-            'fieldName': 'activeprod',
-            'isEditable': true,
-            'tooltip': 'Current Status',
-            'label': 'Status:'
-          }, {
-            'fieldName': 'field_name',
-            'isEditable': true,
-            'tooltip': 'The name of this oil field',
-            'label': 'Field Name:'
-          }, {
-            'fieldName': 'approxacre',
-            'isEditable': false,
-            'label': 'Acreage:'
-          }, {
-            'fieldName': 'avgdepth',
-            'isEditable': false,
-            'label': 'Average Depth:'
-          }, {
-            'fieldName': 'cumm_oil',
-            'isEditable': false,
-            'label': 'Cummulative Oil:'
-          }, {
-            'fieldName': 'cumm_gas',
-            'isEditable': false,
-            'label': 'Cummulative Gas:'
-          }
-        ]
-      }
-    ];
+    var layerInfo, legendDijit;
+    layerInfo = arrayUtils.map(evt.layers, function(layer, index) {
+      return {
+        layer: layer.layer,
+        title: layer.layer.name
+      };
+    });
+    if (layerInfo.length > 0) {
+      legendDijit = new Legend({
+        map: map,
+        layerInfos: layerInfo
+      }, "legendDiv");
+    }
+    return legendDijit.startup();
   });
-  if (layerInfo.length > 0) {
-    console.log(layerInfo);
-    legendDijit = new Legend({
-      map: map,
-      layerInfos: [](layerInfo)
-    }, "legendDiv");
-  }
-  legendDijit.startup();
-  attInspector = new AttributeInspector({
-    layerInfos: layerInfos
-  }, domConstruct.create("div"));
-  map.infoWindow.setContent(attInspector.domNode);
-  layer = new ArcGISDynamicMapServiceLayer(layer_url);
-  return map.addLayers([layer]);
+  return map.addLayers([waterbodies, rivers]);
 });

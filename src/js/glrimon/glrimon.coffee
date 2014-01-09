GreatLakesWetlands/cwmws

commit 2637ad86b396cb20cb92ad92ef88c3a12987753d

site click not working

src/js/glrimon/glrimon.coffee
@@ -16,8 +16,8 @@ centroid_layer = "/0"
 boundary_layer = "/1"
 species_table = "/2"
 
-no_definition_query = '1 = 1'
 no_definition_query = '"site" > 0 and "site" < 10000'
+no_definition_query = '1 = 1'
 # getting "Origin <whatever> is not allowed by Access-Control-Allow-Origin."
 # errors, seems to go away if the definition query is set to this instead
 # of ""?
@@ -178,6 +178,11 @@ require([
         q.geometry = queryGeom
         
         q.where = centroids.getDefinitionExpression()
+        
+        console.log q
+        console.log sites
+        
+        
 
         popupTemplate = new esri.dijit.PopupTemplate
             title: "{site}"

src/js/glrimon/glrimon.js
@@ -28,9 +28,9 @@
 
   species_table = "/2";
 
-  no_definition_query = '1 = 1';
-
   no_definition_query = '"site" > 0 and "site" < 10000';
+
+  no_definition_query = '1 = 1';
 
   /* main
   */
@@ -65,6 +65,8 @@
       q.outFields = ["*"];
       q.geometry = queryGeom;
       q.where = centroids.getDefinitionExpression();
+      console.log(q);
+      console.log(sites);
       popupTemplate = new esri.dijit.PopupTemplate({
         title: "{site}",
         fieldInfos: [

commit 770d16f95178134b7b2fd7829d306e85f3acc832

revert changes in 2637ad86 which broke site info. click those changes were debugging code to work out why site info. click was broken

src/js/glrimon/glrimon.coffee
@@ -202,20 +202,13 @@ require([
             ]
 
         qt = new esri.tasks.QueryTask layer_url + boundary_layer
-        
-        ok = (result) ->
-            console.log 'RS', error
+        def = qt.execute q
+        def.addCallback (result) ->
+
             dojo.map result.features, (f) ->
                 f.setInfoTemplate popupTemplate
                 return f
-                
-        err = (error) ->
-            console.log 'ER', error
-            
-        def = qt.execute q
-        def.addCallback ok
-        def.addErrback err
-        
+
         map.infoWindow.setFeatures [def]
         # show the popup
         map.infoWindow.show e.screenPoint, map.getInfoWindowAnchor e.screenPoint

src/js/glrimon/glrimon.js
@@ -56,7 +56,7 @@
       /* query server for sites near mouseclick
       */
 
-      var def, err, ok, pad, popupTemplate, q, qt, queryGeom;
+      var def, pad, popupTemplate, q, qt, queryGeom;
       pad = map.extent.getWidth() / map.width * 5;
       queryGeom = new esri.geometry.Extent(e.mapPoint.x - pad, e.mapPoint.y - pad, e.mapPoint.x + pad, e.mapPoint.y + pad, map.spatialReference);
       q = new esri.tasks.Query();
@@ -122,19 +122,13 @@
         ]
       });
       qt = new esri.tasks.QueryTask(layer_url + boundary_layer);
-      ok = function(result) {
-        console.log('RS', error);
+      def = qt.execute(q);
+      def.addCallback(function(result) {
         return dojo.map(result.features, function(f) {
           f.setInfoTemplate(popupTemplate);
           return f;
         });
-      };
-      err = function(error) {
-        return console.log('ER', error);
-      };
-      def = qt.execute(q);
-      def.addCallback(ok);
-      def.addErrback(err);
+      });
       map.infoWindow.setFeatures([def]);
       return map.infoWindow.show(e.screenPoint, map.getInfoWindowAnchor(e.screenPoint));
     };